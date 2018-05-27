30c9405d692a5c6c094107414a6114cd1494c5e9
leo <leonardofreitasjc@hotmail.com>
2018-05-27 19:37:50 -0300

    teste

---
+++ celsius.py
@@ -0,0 +1,4 @@
+tmpfahrenheint = input ( "qual a tmp em fahre: ")
+tmpcelsius = (float(tmpfahrenheint)-32) * 5 / 9
+print(tmpcelsius)
+
---
+++ educado.py
@@ -0,0 +1,4 @@
+nomedamae = input("nome da sua mãe: ")
+nomedopai = input("nome do seu pai: ")
+
+print("Bom dia Sr." ,nomedopai, "e Sra.", nomedamae,".")
