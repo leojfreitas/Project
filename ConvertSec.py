530d6f6241e43238556ead1e9efdc6d444169c9e
leo <leonardofreitasjc@hotmail.com>
2018-05-27 19:53:42 -0300

    increment day

--- ConvertSec.py
+++ ConvertSec.py
@@ -1,9 +1,11 @@
 sec_str = input("insert total of sec, please: ")
 total_sec = int(sec_str)
 
+
 hour = total_sec // 3600
+day = hour // 24
 sec_rest = total_sec % 3600
 minut = sec_rest // 60
 sec_rest_final = sec_rest % 60
 
-print("total of sec is" ,hour, "hours, ",minut, "minuts and ", sec_rest_final, "seconds.")
+print("total of sec is",day, "days" ,hour, "hours, ",minut, "minuts and ", sec_rest_final, "seconds.")
