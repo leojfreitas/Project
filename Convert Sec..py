c775b0089a0078bd60898f4cf65170c596a49b70
leonardo <leonardofreitasjc@hotmail.com>
2018-05-27 18:42:12 -0300

    PY
    create Py

---
+++ Convert Sec..py
@@ -0,0 +1,9 @@
+sec_str = input("insert total of sec, please: ")
+total_sec = int(sec_str)
+
+hour = total_sec // 3600
+sec_rest = total_sec % 3600
+minut = sec_rest // 60
+sec_rest_final = sec_rest % 60
+
+print("total of sec is" ,hour, "hours, ",minut, "minuts and ", sec_rest_final, "seconds.")
