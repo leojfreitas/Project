7234d74224c378ebf3bcb97c666570175f55dbad
leo <leonardofreitasjc@hotmail.com>
2018-05-29 20:21:16 -0300

    new class

---
+++ media.py
@@ -0,0 +1,16 @@
+nota1 = input("Digite a primeira nota\n")
+nota2 = input("Digite a segunda nota\n")
+nota3 = input("Digite a terceira nota\n")
+nota4 = input("Digite a quarta nota\n")
+
+
+num1 = float(nota1)
+num2 = float(nota2)
+num3 = float(nota3)
+num4 = float(nota4)
+
+sm = num1+num2+num3+num4
+
+med = sm /4 
+
+print("A média aritmética é" ,med)
